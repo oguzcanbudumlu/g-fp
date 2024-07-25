860b84e57761dda03f2e3186f9883b7617a043bb
Oğuzcan Budumlu <oguzcan.budumlu@trendyol.com>
2024-07-25 20:15:00 +0300

    add

--- type_alias_for_primitive.go
+++ type_alias_for_primitive.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import "fmt"
 
+type phoneNumber string
+type age int
+
+func (a age) valid() bool {
+	return a < 120
+}
+
+func isValidPerson(p Person2) bool {
+	return p.age.valid() && p.name != ""
+}
+
+type Person2 struct {
+	name        string
+	phonenumber phoneNumber
+	age         age
+}
+
+func (p *Person2) setPhoneNumber(s phoneNumber) {
+	p.phonenumber = s
+}
+
+func main() {
+	p := Person2{
+		name:        "John",
+		phonenumber: "123",
+	}
+
+	fmt.Printf("%v\n", p)
 }
