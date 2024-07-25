52225b26e0384628c0c768ce824848da533e07b0
Oğuzcan Budumlu <oguzcan.budumlu@trendyol.com>
2024-07-25 19:27:26 +0300

    add

--- pure_function.go
+++ pure_function.go
@@ -1,5 +1,21 @@
 package main
 
+type Person struct {
+	Age  int
+	Name string
+}
+
+func changeName(p *Person, newName string) {
+	p.Name = newName
+}
+
+func changeNamePure(p Person, newName string) Person {
+	return Person{
+		Age:  p.Age,
+		Name: newName,
+	}
+}
+
 func main() {
 
 }
