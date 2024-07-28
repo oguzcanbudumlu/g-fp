5e2afb48c43a2e1e73e2c196d19cb120b71b6cd3
Oğuzcan Budumlu <obudumlu@gmail.com>
2024-07-28 03:01:31 +0300

    Add partial application

--- partial_application.go
+++ partial_application.go
@@ -12,5 +12,55 @@ func main() {
 	firstGreeting := createGreeting2("Hello there")
 	secondGreeting := createGreeting2("Hola")
 	fmt.Println(firstGreeting("Remi"))
-	fmt.Printf(secondGreeting("Seen"))
+	fmt.Println(secondGreeting("Seen"))
+
+	// spawner
+	bucky := maleHavaneseSpawner("Bucky")
+	rocky := maleHavaneseSpawner("Rocky")
+	tipsy := femalePoodleSpawner("tipsy")
+	fmt.Printf("%v\n%v\n%v\n", bucky, rocky, tipsy)
 }
+
+type (
+	Name        string
+	Breed       int
+	Gender      int
+	NameToDogFn func(Name) Dog
+)
+
+// Possible Breeds
+const (
+	Bulldog Breed = iota
+	Havanese
+	Cavalier
+	Poodle
+)
+const (
+	Male Gender = iota
+	Female
+)
+
+type Dog struct {
+	Name   Name
+	Breed  Breed
+	Gender Gender
+}
+
+func createDogsWithoutParitalApplication() []Dog {
+	return []Dog{
+		{Name: "Bucky", Breed: Havanese, Gender: Male},
+		{Name: "Rocky", Breed: Havanese, Gender: Male},
+		{Name: "Tipsy", Breed: Poodle, Gender: Female},
+	}
+}
+
+func DogSpawner(breed Breed, gender Gender) NameToDogFn {
+	return func(name Name) Dog {
+		return Dog{Breed: breed, Gender: gender, Name: name}
+	}
+}
+
+var (
+	maleHavaneseSpawner = DogSpawner(Havanese, Male)
+	femalePoodleSpawner = DogSpawner(Poodle, Female)
+)
